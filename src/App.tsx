3a0afb39328431b5d4d40a301051bc79bbf0f258
Giuseppe Pesce <giuseppe.pesce@termo.com>
2025-07-21 11:30:37 +0200

    add footer component and import on app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css';
+import Header from './components/Header';
+
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
-      <div>header</div>
+      <Header />
       <div>main content</div>
-      <div>footer</div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+   return <footer className="bg-gray-200 p-4 text-center text-sm text-gray-600">
+      © {new Date().getFullYear()} Little Lemon. Tutti i diritti riservati.
+    </footer>
+};
+
+export default Footer;
