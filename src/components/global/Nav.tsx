a88bc988e604858a71fb6b15c6969b0979444561
Giuseppe Pesce <giuseppe.pesce@termostore.it>
2025-09-16 16:37:36 +0200

    fix automatically close mobile menu on link click

--- src/components/global/Nav.tsx
+++ src/components/global/Nav.tsx
@@ -2,24 +2,25 @@ import {
   NavigationMenu,
   NavigationMenuList,
   NavigationMenuItem,
-  NavigationMenuLink
-} from '@/components/ui/navigation-menu';
+  NavigationMenuLink,
+} from "@/components/ui/navigation-menu";
 import {
   Sheet,
+  SheetClose,
   SheetContent,
   SheetHeader,
   SheetTitle,
-  SheetTrigger
-} from '@/components/ui/sheet';
-import { Menu } from 'lucide-react';
-import { Button } from '@/components/ui/button';
-import { cn } from '@/lib/utils';
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
 const links = [
-  { href: '/#chi-siamo', label: 'Chi siamo' },
-  { href: '/#menu', label: 'Menu' },
-  { href: '/#contatti', label: 'Contatti' },
-  { href: '/prenotazione', label: 'Prenota' }
+  { href: "/#chi-siamo", label: "Chi siamo" },
+  { href: "/#menu", label: "Menu" },
+  { href: "/#contatti", label: "Contatti" },
+  { href: "/prenotazione", label: "Prenota" },
 ];
 
 const Nav = () => {
@@ -41,8 +42,8 @@ const Nav = () => {
                   <NavigationMenuLink
                     href={link.href}
                     className={cn(
-                      link.label === 'Prenota'
-                        ? 'bg-secondary text-primary px-3 py-2 font-bold rounded-lg text-lg hover:bg-secondary/80 transition'
+                      link.label === "Prenota"
+                        ? "bg-secondary text-primary px-3 py-2 font-bold rounded-lg text-lg hover:bg-secondary/80 transition"
                         : linkStyle
                     )}
                   >
@@ -73,13 +74,15 @@ const Nav = () => {
               </SheetHeader>
               <ul className="px-4 py-8 mt-4 space-y-4 bg-green-950 h-screen">
                 {links.map((link) => (
-                  <li key={link.href} className='py-6 '>
-                    <a
-                      href={link.href}
-                      className="block text-2xl font-medium hover:text-primary border-b-1 border-white pb-2"
-                    >
-                      {link.label}
-                    </a>
+                  <li key={link.href} className="py-6 ">
+                    <SheetClose asChild>
+                      <a
+                        href={link.href}
+                        className="block text-2xl font-medium hover:text-primary border-b-1 border-white pb-2"
+                      >
+                        {link.label}
+                      </a>
+                    </SheetClose>
                   </li>
                 ))}
               </ul>
@@ -92,7 +95,7 @@ const Nav = () => {
 };
 
 const linkStyle = cn(
-  'text-xl font-medium transition-colors hover:text-primary'
+  "text-xl font-medium transition-colors hover:text-primary"
 );
 
 export default Nav;
